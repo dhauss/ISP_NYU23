417080eddacf4a3ab4dac2e3bb56aa3dc27bc5e6
dhauss <davidkhauss@gmail.com>
2023-10-10 13:17:14 -0400

    added ranged for loop to bash script

--- Assignment_2/run_tests.sh
+++ Assignment_2/run_tests.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-for i in 1 2 3 4
+for i in {1..4}
 do
     python repy.py restrictions.default encasementlib.r2py reference_monitor_dh3382.r2py reference_monitor_attack${i}_dh3382.r2py
 done   
