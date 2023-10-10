cb242a1577f2e2c1adff18b62a829a27e488058a
dhauss <davidkhauss@gmail.com>
2023-10-10 13:15:27 -0400

    for loop added to bash script

--- Assignment_2/run_tests.sh
+++ Assignment_2/run_tests.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-python repy.py restrictions.default encasementlib.r2py reference_monitor_dh3382.r2py reference_monitor_attack1_dh3382.r2py
-python repy.py restrictions.default encasementlib.r2py reference_monitor_dh3382.r2py reference_monitor_attack2_dh3382.r2py
-python repy.py restrictions.default encasementlib.r2py reference_monitor_dh3382.r2py reference_monitor_attack3_dh3382.r2py
-python repy.py restrictions.default encasementlib.r2py reference_monitor_dh3382.r2py reference_monitor_attack4_dh3382.r2py
+for i in 1 2 3 4
+do
+    python repy.py restrictions.default encasementlib.r2py reference_monitor_dh3382.r2py reference_monitor_attack${i}_dh3382.r2py
+done   
