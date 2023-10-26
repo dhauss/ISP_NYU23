a6e3c8a2857c3b00c223c5aa2f6edcc45145892a
dhauss <davidkhauss@gmail.com>
2023-10-26 14:13:30 -0400

    added attack cases

--- Assignment_2/run_attacks.sh
+++ Assignment_2/run_attacks.sh
@@ -3,8 +3,9 @@
 for referencemonitor in reference_monitor_*
 do
     echo $referencemonitor under test
-    for testcase in dh3382_*
-    do
-        python repy.py restrictions.default encasementlib.r2py $referencemonitor $testcase
-    done
+    python repy.py restrictions.default encasementlib.r2py $referencemonitor dh3382_attackcase5.r2py
+    # for testcase in dh3382_*
+    # do
+    #    python repy.py restrictions.default encasementlib.r2py $referencemonitor $testcase
+    # done
 done
